74f2dae0f5ab68b297f50c367e20e25e164b57e4
Andre-art <demoura.as@gmail.com>
2024-02-20 23:08:12 -0300

    add

--- src/Lib/createClient.js
+++
@@ -1,8 +0,0 @@
-import * as contentful from 'contentful';
- 
-import { spaceId, accessToken } from '../constants';
- 
-export const client = contentful.createClient({
-    space: spaceId,
-    accessToken: accessToken,
-});
